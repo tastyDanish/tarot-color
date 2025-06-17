8017379cb2b67e493382e96cf4d9a0c9aa7f80ca
Peter Lansdaal <peter@gmail.com>
2025-06-17 16:47:53 -0700

    update: some styling for when gradient is not supported

--- src/share/index.tsx
+++ src/share/index.tsx
@@ -8,7 +8,7 @@ const ShareButton = () => {
     <button
       disabled={toastOpen}
       onClick={handleShare}
-      className="flex items-center gap-2 px-3 py-2 bg-gradient-to-r from-orange-200 to-indigo-200 text-slate-900 rounded-full shadow-md transition hover:scale-105 w-fit h-fit text-md">
+      className="inline-flex items-center gap-2 px-3 py-2 bg-orange-200 bg-gradient-to-r from-orange-200 to-indigo-200 text-slate-900 rounded-full shadow-md border border-indigo-300 transition hover:scale-105 text-md">
       <Sparkles className="w-4 h-4 sm:w-5 sm:h-5" />
       <span className="relative w-[50px] h-[1em] flex items-center justify-center">
         Share
